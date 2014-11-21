1e0c30dfc6720552aba008abf6343218ac47e21c
brakmic <brakmic@gmail.com>
2014-11-21 18:16:55 +0100

    'clenup'

--- Scripts/app/demo.model.js
+++ Scripts/app/demo.model.js
@@ -2,25 +2,28 @@
 
     var app = global.app = global.app || {};
 
-    //predefine a mithril-based model for json data we're going to receive
-    var Customer = function (jsonData) {
-        var data = jsonData.value[0];
-        this.id = m.prop(jsonData.CustomerID);
-        this.name = m.prop(jsonData.ContactName);
-        this.company = m.prop(jsonData.CompanyName);
-        this.address = m.prop(jsonData.Address);
-        this.city = m.prop(jsonData.City);
-        this.postalCode = m.prop(jsonData.PostalCode);
-        this.country = m.prop(jsonData.Country);
-        this.phone = m.prop(jsonData.Phone);
-    };
-
+    //Here we use Mithrils "request" method to connect the Service and get the data.
+    //Optionally we could predefine a "model class" and let Mithril process data according to its properties.
+    //For example:
+    //var Customer = {
+    //      id: jsonData.CustomerID,
+    //      name: jsonData.CustomerName,
+    //      company: jsonData.CompanyName
+    //  }
+    //
+    //then we'd call the same m.request method with an additional parameter: "type":
+    //      m.request = ( method: "GET", url: "service_url", type: Customer )
+    //
+    //Mithril would then use this class to preprocess received data
+    //
+    //But in this example we will use Kendo-Framework's DataSource functionality to preprocess JSON data
+    //More info here: http://docs.telerik.com/kendo-ui/api/javascript/data/datasource
+    //
     app.Customers = function () {
         return m.request(
             {
                 method: "GET",
                 url: 'http://services.odata.org/Northwind/Northwind.svc/Customers?$format=json',
-                //type: Customer //cast the received json payload to "Customer"
             });
     };
 
